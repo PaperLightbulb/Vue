79716f6f8fdd41db0f6a21f32cacc30ed1ad14c1
Falafel <tadgranite@gmail.com>
2023-11-27 08:14:13 -0600

    got site getting fixed

--- vue-testing/src/App.vue
+++ vue-testing/src/App.vue
@@ -1,85 +1,71 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+  import { VueElement, ref } from 'vue'
+
+
+  const counter = ref(0)
+  const flipText = ref("Flip")
+  const pg = ref("")
+  const a = ref("jdfksal")
+
+
+
+
+  function incrementCounter () {
+    counter.value++
+  }
+  function flip () {
+    flipText.value = flipText.value.split("").reverse().join("")
+    setA(a.value + "a")
+  }
+  function setA (str) {
+    a.value = str
+  }
+  function getSite () {
+    axios.get(url).then((request) => {
+      setA(request.data)
+    }).catch((error) => {
+      console.log(error)
+    })
+  }
+</script>
+
+<script>
+  import axios from 'axios'
+
+  const url = 'https://flaskapitest.onrender.com/'; 
+
+  
+
+  export default {
+    name: 'App'
+  }
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <div>
+    <h2>P:{{ pg }}</h2>
+    <h3>{{ a }}</h3>
+    <button @click="incrementCounter">Counter: {{ counter }}</button>
+    <button @click="flip">{{ flipText }}</button>
+    <button @click="getSite">Get Site</button>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+<style>
+div {
+  display: flex;
+  flex-direction: column;
+  width: 100vw;
+  height: 100vh;
+  align-items: center;
+  justify-content: center;
 }
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+button {
+  background-color: black;
+  color: white;
+  border-radius: 50vh;
+  margin: .5%;
+  font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  font-size: 14pt;
 }
 </style>
